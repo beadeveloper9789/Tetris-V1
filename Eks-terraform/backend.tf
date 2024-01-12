5d0fc7ceb296c85bd18b86a3f063264ad61963ce
beadeveloper9789 <148839853+beadeveloper9789@users.noreply.github.com>
2024-01-12 10:23:52 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "queuefirst" # Replace with your actual S3 bucket name
-    key    = "naresh/terraform.tfstate"
+    key    = "ram/terraform.tfstate"
     region = "us-west-2"
   }
 }
